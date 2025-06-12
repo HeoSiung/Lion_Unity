479128744563a657cbef1ce5bb7cfc6860e25cd3
HeoSiung <heosiung02@gmail.com>
2025-06-12 16:18:13 +0900

    [ADD]
    2025-06-12

---
+++ Assets/02. Scripts/Cat/ColliderEvent.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ColliderEvent : MonoBehaviour
+{
+    public GameObject fadeUI;
+    
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            fadeUI.SetActive(true);
+        }
+    }
+}
---
+++ Assets/02. Scripts/Cat/GameManager.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEngine;
+
+namespace Cat
+{
+    public class GameManager : MonoBehaviour
+    {
+        public SoundManager soundManager;
+
+        public TextMeshProUGUI playTimeUI;
+        public TextMeshProUGUI scoreUI;
+
+        private float timer;
+        public static int score; // 사과 먹은 갯수
+        public static bool isPlay;
+
+        void Start()
+        {
+            soundManager.SetBGMSound("Intro");
+        }
+
+        void Update()
+        {
+            if (!isPlay) 
+                return;
+
+            timer += Time.deltaTime;
+
+            playTimeUI.text = $"플레이 시간 : {timer:F1}초";
+            scoreUI.text = $"<color=red>X</color> {score}";
+        }
+    }
+}
