4f5833503cb4b2e1f17230fea942aae02e64a856
HeoSiung <heosiung02@gmail.com>
2025-06-27 16:27:09 +0900

    [ADD] 2025-06-27
     2025-06-27

---
+++ Assets/02. Scripts/Knight Platformer/InteractionEvent.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class InteractionEvent : MonoBehaviour
+{
+    public enum InteractionType { SIGN, DOOR, NPC }
+    public InteractionType type;
+
+    public SoundController soundController;
+
+    public GameObject popUp;
+
+    public DarkerRoutine fade;
+
+    public GameObject map;
+    public GameObject house;
+
+    public Vector3 inDoorPos;
+    public Vector3 outDoorPos;
+    public bool isHouse;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Interaction(other.transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            popUp.SetActive(false);
+        }
+    }
+
+    void Interaction(Transform player)
+    {
+        switch (type)
+        {
+            case InteractionType.SIGN:
+                popUp.SetActive(true);
+                break;
+            case InteractionType.DOOR:
+                StartCoroutine(DoorRoutine(player));
+                break;
+            case InteractionType.NPC:
+                popUp.SetActive(true);
+                break;
+        }
+    }
+
+    IEnumerator DoorRoutine(Transform player)
+    {
+        soundController.EventSoundPlay("Door Open");
+
+        yield return StartCoroutine(fade.Fade(2f, Color.black, true));
+
+        map.SetActive(isHouse);
+        house.SetActive(!isHouse);
+
+        var pos = isHouse ? outDoorPos : inDoorPos;
+        player.transform.position = pos;
+
+        isHouse = !isHouse;
+
+        yield return new WaitForSeconds(1f);
+        soundController.EventSoundPlay("Door Close");
+
+        yield return StartCoroutine(fade.Fade(2f, Color.black, false));
+    }
+}
